af58305707bf6b0554a59a5f30cea80258ec1a7b
zillur-rgb <zillurdeu@gmail.com>
2022-04-07 14:23:10 +0200

    Tailwind css and Daisy UI implemented

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Nav.css";
+import { MdLocalMovies } from "react-icons/md";
+
+const Nav = () => {
+  return (
+    <div>
+      <MdLocalMovies style={{ color: "#fff", fontSize: "2rem" }} />
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/Components/Trending/Trending.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Trending = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default Trending;
